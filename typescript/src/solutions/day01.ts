0ffbba935921c4ad5199b0e7b6a3caae03dd0955
Ben Albin <ben.g.albin@gmail.com>
2018-12-01 20:56:23 +0000

    Typescript solution

---
+++ typescript/src/solutions/day01.ts
@@ -0,0 +1,28 @@
+import { readFileSync } from "fs";
+
+const args: ReadonlyArray<String> = readFileSync(
+  "./src/resources/day01.txt",
+  "utf8"
+).split("\n");
+
+function part1(args: ReadonlyArray<String>): number {
+  return args.reduce((acc, val) => acc + +val, 0);
+}
+
+function part2(args: ReadonlyArray<String>): number {
+  let seen = new Set([0]);
+  let freq: number = 0;
+  while (true) {
+    for (let i: number = 0; i < args.length - 1; i++) {
+      freq += +args[i];
+      if (seen.has(freq)) {
+        return freq;
+      }
+      seen.add(freq);
+    }
+  }
+  return 0;
+}
+
+console.log("Part 1 Results: " + part1(args));
+console.log("Part 2 Results: " + part2(args));
